26b93d1f8ff8510f7011fe546e9e8a53c8827dd7
Casquilho <jm.casquilho@campus.fct.unl.pt>
2020-08-01 01:53:43 +0100

    terrain generation base

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class Camera_Follow : MonoBehaviour
 {
     public Transform player;
+    //private Rigidbody2D rb;
+    
     public int zOffSet = 5;
+    public int xOffSet = 0;
+    public int yOffSet = 0;
+    //Transform currentView;
+    //public int transitionSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb = player.gameObject.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, player.position.z-zOffSet);
+        transform.position = new Vector3(player.position.x - xOffSet, player.position.y - yOffSet, player.position.z - zOffSet);
+        /*if(rb.velocity.x >= 0)
+            currentView.position = new Vector3(player.position.x - xOffSet, player.position.y - yOffSet, player.position.z - zOffSet);
+        
+        else
+            currentView.position = new Vector3(player.position.x + xOffSet, player.position.y - yOffSet, player.position.z-zOffSet);
+
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);*/
     }
+
+    /*void LateUpdate()
+    {
+
+        //Lerp position
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+         Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        transform.eulerAngles = currentAngle;
+
+    }*/
 }
---
+++ Assets/TerrainGeneration.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public int distance;
+    public int space;
+
+    public GameObject Grass;
+    public GameObject Dirt;
+    public GameObject Stone;
+
+    public float heightpoint;
+    public float heightpoint2;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Stone.layer = 9;
+        Grass.layer = 9;
+        Dirt.layer = 9;
+        Generation();
+    }
+
+    void Generation()
+    {
+        distance = height;
+        for (int w = 0; w < width; w++)
+        {
+           
+            int lowernum = distance - 1;
+            int heighernum = distance + 2;
+            distance = Random.Range(lowernum, heighernum);
+            space = Random.Range(12, 20);
+            int stonespace = distance - space;
+
+
+            for (int j = 0; j < stonespace; j++)
+            {
+                Instantiate(Stone, new Vector3(w, j), Quaternion.identity);
+            }
+
+            for (int j = stonespace; j < distance; j++)
+            {
+                Instantiate(Dirt, new Vector3(w, j), Quaternion.identity);
+            }
+            Instantiate(Grass, new Vector3(w, distance), Quaternion.identity);
+
+
+
+
+
+            int i = 0;
+            for (i = distance +10 ; i < distance + 10 + stonespace; i++)
+            {
+                Instantiate(Stone, new Vector3(w, i), Quaternion.identity);
+            }
+
+            for (int j = i; j < i+stonespace; j++)
+            {
+                Instantiate(Dirt, new Vector3(w, j), Quaternion.identity);
+            }
+            
+        }
+    }
+}
